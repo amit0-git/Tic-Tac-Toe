5b4a372096ee1a9e77acface46585482d618d393
Amit Verma <root.avanti@gmail.com>
2025-05-08 09:52:33 +0530

    dfdf

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const http = require("http")
 const socketIO = require("socket.io")
-const cors = require("cors")
 const path = require('path'); 
 const app = express()
 
